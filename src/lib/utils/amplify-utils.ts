529ed622c455f471f6a30a8a663093388b78ad45
henhal <henhalvor@gmail.com>
2024-12-05 18:22:13 +0100

    Add req / res based client

--- src/lib/utils/amplify-utils.ts
+++ src/lib/utils/amplify-utils.ts
@@ -1,14 +1,22 @@
 // utils/amplify-utils.ts
+// https://docs.amplify.aws/nextjs/build-a-backend/data/connect-from-server-runtime/nextjs-server-runtime/
 import { cookies } from "next/headers";
 
 import { createServerRunner } from "@aws-amplify/adapter-nextjs";
-import { generateServerClientUsingCookies } from "@aws-amplify/adapter-nextjs/api";
+import {
+  generateServerClientUsingCookies,
+  generateServerClientUsingReqRes,
+} from "@aws-amplify/adapter-nextjs/api";
 import { getCurrentUser } from "aws-amplify/auth/server";
 
 import { type Schema } from "@/../amplify/data/resource";
 import outputs from "@/../amplify_outputs.json";
 
 export const { runWithAmplifyServerContext } = createServerRunner({
+  config: outputs,
+});
+
+export const reqResBasedClient = generateServerClientUsingReqRes<Schema>({
   config: outputs,
 });
 
